017b20605a93b529af158ac358dc4f0d0c7065fd
eunjiruss <31670138+eunjiruss@users.noreply.github.com>
2017-11-03 15:48:28 -0700

    more working things.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,20 +7,17 @@
 //The game loads after sencond alert"//
 
 //Define variables//
-var userAnswer = 0;
-var answers = [];
 var correctAnswer = 0;
 var incorrectAnswer = 0;
-
 var numRounds = 0;
 
 //console.log(question);
 //shows as undefined at this point//
 var questions = [
-	new TriviaQuestion("number1?", ["This is a random", "B", "C", "D"], "A"),
-	new TriviaQuestion("number2?", ["lol", "B", "C", "D"], "B"),
-	new TriviaQuestion("number3?", ["jk", "B", "C", "D"], "C"),
-	new TriviaQuestion("number4?", ["A", "B", "C", "D"], "D"),
+	new TriviaQuestion("number1?", ["This is a random", "B", "C", "D"], 0),
+	new TriviaQuestion("number2?", ["lol", "B", "C", "D"], 1),
+	new TriviaQuestion("number3?", ["jk", "B", "C", "D"], 2),
+	new TriviaQuestion("number4?", ["A", "B", "C", "D"], 3),
 ];
 
 var quiz = null;
@@ -36,20 +33,22 @@ function TriviaPlay(questions) {
 function TriviaQuestion(text, choices, answer) {
     this.text = text;
     this.choices = choices;
-    this.answer = answer;
+    this.answer = choices[answer];
 }
 
 function renderHTML() {
-	$("#choice1").html(quiz.selected.choices[0]);
-	$("#choice2").html(quiz.selected.choices[1]);
-	$("#choice3").html(quiz.selected.choices[2]);
-	$("#choice4").html(quiz.selected.choices[3]);
+	// Only write the html if the selected answer is valid.
+	// This can become undefined when we call renderHtml with
+	// numrounds > choices.length(). For instance, if the user
+	// keeps clicking on the buttons.
+	if (quiz.selected !== undefined) {
+		$("#choice1").html(quiz.selected.choices[0]);
+		$("#choice2").html(quiz.selected.choices[1]);
+		$("#choice3").html(quiz.selected.choices[2]);
+		$("#choice4").html(quiz.selected.choices[3]);
 
-	$(".question").html(quiz.selected.text);
-}
-
-function onEndGame() {
-	$("#roundButton").show();
+		$(".question").html(quiz.selected.text);
+	}
 }
 
 function showQuestion() {
@@ -58,28 +57,44 @@ function showQuestion() {
 }
 
 function onButtonClick(num) {
+
+	// If a button is clicked after we've shown all
+	// the questions, just return early and make sure
+	// the button to restart is shown.
+	if (numRounds >= questions.length) {
+		$("#roundButton").show();
+		return;
+	}
+
+	// The rest of this code:
+	//. - bumps up the number of rounds.
+	//. - writes out the number of correct/incorrect answers.
+	//. - writes out the real answer.
+	//. - displays the next question.
+
+	++numRounds;
+
 	if(quiz.selected.choices[num-1] == quiz.selected.answer) {
 		correctAnswer++;
+		$("#correctAnswer").html("Correct: " + correctAnswer);
 	}
 	else {
 		incorrectAnswer++;
+		$("#incorrectAnswer").html("Incorrect: " + incorrectAnswer);
 	}
 
-	$(".answers").html(quiz.selected.answer);
-
-	++numRounds
-	if (numRounds >= questions.length) {
-		onEndGame();
-		return;
-	}
+	$(".answers").html("Real Answer: "  + quiz.selected.answer);
 
 	showQuestion();
 }
 
 function registerEventListeners() {
+
+	// Everything here needs to be installed **after** the DOM is loaded.
+	// which is why they are all in this function.
+
 	$( "#roundButton" ).click(function() {
-		numRounds = 0;
-		showQuestion();
+		location.reload();
 	});
 	$( "#roundButton").hide();
 
